ab301354994db8bac8d00889ae050c52b625bff4
Nate Card <ncard24@gmail.com>
2024-04-29 14:19:35 -0400

    Refactor NotionExporter class to use create a new database for each book, each book gets its own highlight database now

--- src/kobo_highlights_export/notion.py
+++ src/kobo_highlights_export/notion.py
@@ -1,16 +1,40 @@
 from notion_client import Client
 
+
 class NotionExporter:
     def __init__(self, token, db_id):
-        self.notion = Client(token=token)
+        self.notion = Client(auth=token)
         self.db_id = db_id
-        
-    def add_highlight(self, author, title, text):
+
+    def create_book_database(self, title, author):
+        new_database = {
+            "parent": {"page_id": self.db_id},
+            "title": [
+                {
+                    "type": "text",
+                    "text": {
+                        "content": f"{title} by {author}",
+                    },
+                }
+            ],
+            "properties": {
+                "Text": {"title": {}},
+                "Book Progress": {"number": {}},
+                "Date Created": {"date": {}},
+            },
+        }
+        response = self.notion.databases.create(**new_database)
+        return response["id"]
+
+    def add_highlight(self, book_db_id, text, book_progress, date_created):
         new_page = {
-            "parent": {"database_id": self.db_id},
-            "properties":{
-                "Author": {"title": [{"text": {"content": author}}]},
-                "Book Title": {"rich_text": [{"text": {"content": title}}]},
-                "Text": {"rich_text": [{"text": {"content": text}}]}
-        }}
-        self.client.pages.create(**new_page)
+            "parent": {"database_id": book_db_id},
+            "properties": {
+                # "Author": {"title": [{"text": {"content": author}}]},
+                # "Book Title": {"rich_text": [{"text": {"content": title}}]},
+                "Text": {"title": [{"text": {"content": text}}]},
+                "Book Progress": {"number": book_progress},
+                "Date Created": {"date": {"start": date_created}},
+            },
+        }
+        self.notion.pages.create(**new_page)
