f5ca82415fa43b2092063d17ceac01bca700ce70
Nate Card <ncard24@gmail.com>
2024-04-29 10:35:15 -0400

    Refactor main.py script to use NotionExporter class and update block creation logic. Delete original export.py file due to project restructure.

--- src/kobo_highlights_export/main.py
+++ src/kobo_highlights_export/main.py
@@ -4,6 +4,8 @@ from os import environ
 from dotenv import load_dotenv
 from notion_client import Client
 
+from src.kobo_highlights_export.notion import NotionExporter
+
 load_dotenv()
 notion_api_key = environ.get('NOTION_API_KEY')
 notion_db_id = environ.get('NOTION_DB_ID')
@@ -25,7 +27,12 @@ def main():
         book = Utils.extract_book(highlight)
         text = Utils.extract_text(highlight)
         print(author, book, text)
-
+    notion = NotionExporter(notion_api_key, notion_db_id)
+    for highlight in highlights:
+        author = Utils.extract_author(highlight)
+        book = Utils.extract_book(highlight)
+        text = Utils.extract_text(highlight)
+        notion.add_highlight(author, book, text)
 
 if __name__ == "__main__":
     main()
