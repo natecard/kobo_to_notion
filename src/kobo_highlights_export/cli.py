61f381f2d706672ad04956f2547f393dd19ac482
Nate Card <ncard24@gmail.com>
2024-05-01 22:08:33 -0400

    Refactor cli.py to handle API keys as secrets include better help information.

--- src/kobo_highlights_export/cli.py
+++ src/kobo_highlights_export/cli.py
@@ -1,28 +1,40 @@
 import click
 from kobo import KoboDatabase
 from notion import NotionExporter
-from src.kobo_highlights_export.notion_insert import NotionInsert
+from notion_insert import NotionInsert
 
 
 @click.group()
 def cli():
-    """Command line tool for importing Kobo e-reader highlights into a Notion database"""
+    """
+    Command line tool for importing Kobo e-reader highlights into a Notion database.
+    This tool allows you to connect your Kobo device, download highlights, and export them to a specified Notion database.
+    """
     pass
+
+
+click.Context(command=cli, max_content_width=1000)
 
 
 @cli.command()
 @click.option(
     "--filepath",
     prompt="File path to your Kobo",
-    help="""Connect your Kobo to your computer and note the file path to the Kobo, you can copy and paste the file path here. It should look something like D:/KOBOeReader or /Volumes/KOBOeReader""",
+    help="Connect your Kobo to your computer and note the file path to the Kobo, you can copy and paste the file path here. It should look something like D:/KOBOeReader or /Volumes/KOBOeReader. This path is used to access the SQLite database containing your highlights.",
+    type=click.Path(exists=True),
 )
 @click.option(
-    "--api-key", prompt="Your Notion API key", help="Enter your Notion API key."
+    "--api-key",
+    prompt="Your Notion API key",
+    help="Enter your Notion API key. This key is necessary for authentication to access your Notion workspace.",
+    hide_input=True,
+    confirmation_prompt=True,
 )
 @click.option(
     "--db-id",
     prompt="Your Notion Database ID",
-    help="Enter the id of the Notion database where you want to export the highlights",
+    help="Enter the ID of the Notion database where you want to export the highlights. Ensure that this database is set up to receive the data structure being exported.",
+    confirmation_prompt=True,
 )
 def process_books(filepath, notion_api_key, notion_db_id):
     """Retrieve database from the Bookmarks SQLite database from the Kobo."""
