061493c626099f66403d4bb9f68401bc9568b74d
Nate Card <ncard24@gmail.com>
2024-05-01 21:33:21 -0400

    Refactor cli.py to rename database_path function to set_filepath and add notion_setup command

--- src/kobo_highlights_export/cli.py
+++ src/kobo_highlights_export/cli.py
@@ -15,7 +15,7 @@ def cli():
     prompt="File path to your Kobo",
     help="""Connect your Kobo to your computer and note the file path to the Kobo, you can copy and paste the file path here. It should look something like D:/KOBOeReader or /Volumes/KOBOeReader""",
 )
-def database_path(filepath):
+def set_filepath(filepath):
     """Retrieve database from the Bookmarks SQLite database from the Kobo."""
     kobo_db = KoboDatabase(filepath)
     kobo_db.connect()
@@ -24,3 +24,18 @@ def database_path(filepath):
     print(books)
     kobo_db.close()
     click.echo("Kobo database connection complete.")
+
+
+@cli.command()
+@click.option(
+    "--api-key", prompt="Your Notion API key", help="Enter your Notion API key."
+)
+@click.option(
+    "--db-id",
+    prompt="Your Notion Database ID",
+    help="Enter the id of the Notion database where you want to export the highlights",
+)
+def notion_setup(api_key, db_id):
+    """Set up the Notion exporter with API key and database ID"""
+    notion_exporter = NotionExporter(api_key, db_id)
+    click.echo(f"Notion setup complete with Database ID: {db_id}")
