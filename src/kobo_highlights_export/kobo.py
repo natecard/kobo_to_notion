3062080cac9eaaa769bc00a261b56d9a7acc26c5
Nate Card <ncard24@gmail.com>
2024-05-02 11:48:50 -0400

    Refactor KoboDatabase class to handle database connection errors and closing properly

--- src/kobo_highlights_export/kobo.py
+++ src/kobo_highlights_export/kobo.py
@@ -5,27 +5,46 @@ from utils import extract_author, extract_book, extract_text, round_progress
 
 class KoboDatabase:
     def __init__(self, db_path):
-        ereader_db = os.path.join(db_path, "./kobo/KoboReader.sqlite")
-        self.db_path = ereader_db
+        if "KoboReader.sqlite" not in db_path:
+            db_path = os.path.join(db_path, "./kobo/KoboReader.sqlite")
+        self.db_path = db_path
 
     def connect(self):
-        self.conn = sqlite3.connect(self.db_path)
-        self.conn.row_factory = sqlite3.Row
-        self.cursor = self.conn.cursor()
+        try:
+            self.conn = sqlite3.connect(self.db_path)
+            self.conn.row_factory = sqlite3.Row
+            self.cursor = self.conn.cursor()
+        except Exception as e:
+            print(f"Error connecting to Kobo database: {e}")
+            print(
+                "Please ensure the correct file path to the Kobo database is provided."
+            )
+            return None
 
     def close(self):
-        self.cursor.close()
-        self.conn.close()
+        try:
+            self.cursor.close()
+            self.conn.close()
+        except Exception as e:
+            print(f"Error closing Kobo database connection: {e}")
+            print(
+                "Kobo database connection still active. Please attempt closure again."
+            )
+            return None
 
     def get_highlights(self):
-        query = """select BookmarkID, VolumeID, ContentID, 
-                        Text, Annotation, Type, DateCreated, DateModified, ChapterProgress
-                        from Bookmark
-                        where Type IN ("highlight", "note")
-                        order by VolumeId, ContentId, ChapterProgress """
-        self.cursor.execute(query)
-        rows = self.cursor.fetchall()
-        books = {}
+        try:
+            query = """select BookmarkID, VolumeID, ContentID, 
+                            Text, Annotation, Type, DateCreated, DateModified, ChapterProgress
+                            from Bookmark
+                            where Type IN ("highlight", "note")
+                            order by VolumeId, ContentId, ChapterProgress """
+            self.cursor.execute(query)
+            rows = self.cursor.fetchall()
+            books = {}
+        except Exception as e:
+            print(f"Error retrieving highlights: {e}")
+            return None
 
         for row in rows:
             book_unformatted = row["VolumeID"]
