72dcafb74981c6811424f2436baa9773edd2a1b8
Nate Card <ncard24@gmail.com>
2024-05-02 12:03:29 -0400

    Refactor cli.py to improve help information for user input, set to echo filepath and database id. Removed excess code from main.py

--- src/kobo_highlights_export/cli.py
+++ src/kobo_highlights_export/cli.py
@@ -18,25 +18,32 @@ click.Context(command=cli, max_content_width=1000)
 
 @cli.command()
 @click.option(
+    "-Filepath",
     "--filepath",
-    prompt="File path to your Kobo",
+    "-filepath",
+    prompt="Enter the file path to your Kobo",
     help="Connect your Kobo to your computer and note the file path to the Kobo, you can copy and paste the file path here. It should look something like D:/KOBOeReader or /Volumes/KOBOeReader. This path is used to access the SQLite database containing your highlights.",
     type=click.Path(exists=True),
 )
 @click.option(
+    "-API-Key",
     "--api-key",
-    prompt="Your Notion API key",
+    "-api-key",
+    prompt="Enter your Notion API key",
     help="Enter your Notion API key. This key is necessary for authentication to access your Notion workspace.",
     hide_input=True,
     confirmation_prompt=True,
 )
 @click.option(
+    "-Database-ID",
     "--db-id",
-    prompt="Your Notion Database ID",
+    "-db-id",
+    prompt="Enter your Notion Database ID",
     help="Enter the ID of the Notion database where you want to export the highlights. Ensure that this database is set up to receive the data structure being exported.",
-    confirmation_prompt=True,
 )
-def process_books(filepath, notion_api_key, notion_db_id):
+def process_books(filepath, api_key, db_id):
+    click.echo(f"Database ID: {db_id}")
+    click.echo(f"File Path: {filepath}")
     """Retrieve database from the Bookmarks SQLite database from the Kobo."""
     kobo_db = KoboDatabase(filepath)
     kobo_db.connect()
@@ -46,8 +53,8 @@ def process_books(filepath, notion_api_key, notion_db_id):
     click.echo("Kobo database connection complete.")
 
     """Set up the Notion exporter with API key and database ID"""
-    notion_exporter = NotionExporter(notion_api_key, notion_db_id)
-    click.echo(f"Notion setup complete with Database ID: {notion_db_id}")
+    notion_exporter = NotionExporter(api_key, db_id)
+    click.echo(f"Notion setup complete with Database ID: {db_id}")
     book_processor = NotionInsert(notion_exporter)
     book_processor.process_books(books)
     click.echo("Books successfully processed and added to Notion database.")
--- src/kobo_highlights_export/main.py
+++ src/kobo_highlights_export/main.py
@@ -1,43 +1,4 @@
-import os
-from dotenv import load_dotenv
-from notion_client import Client
-
 from cli import cli
-from kobo import KoboDatabase
-from notion import NotionExporter
-
-
-load_dotenv()
-notion_api_key = os.environ.get("NOTION_API_KEY")
-notion_db_id = os.environ.get("NOTION_DB_ID")
-notion = Client(auth=notion_api_key)
-if os.environ.get("PROD") is True:
-    ereader_db = os.environ.get("EREADER_DB")
-else:
-    db_path = os.environ.get("DEV_EREADER_DB")
-    ereader_db = os.path.join(os.path.dirname(__file__), "..", "..", db_path)
-
-
-def main():
-    kobo_db = KoboDatabase(ereader_db)
-    kobo_db.connect()
-    books = kobo_db.get_highlights()
-    print(books)
-    kobo_db.close()
-
-    notion = NotionExporter(notion_api_key, notion_db_id)
-
-    for book, book_info in books.items():
-        book_db_id = notion.create_book_database(book, book_info["author"])
-
-        for highlight in book_info["highlights"]:
-            notion.add_highlight(
-                book_db_id,
-                highlight["text"],
-                highlight["chapter_progress"],
-                highlight["date_created"],
-            )
-
 
 if __name__ == "__main__":
     cli()
