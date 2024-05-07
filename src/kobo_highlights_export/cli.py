c0d6700c58261e3c5c456f3adb911b966cc48e4a
Nate Card <ncard24@gmail.com>
2024-05-07 16:07:53 -0400

    Refactor cli.py so user input is handled within the process_book function. Removed development code from main.py

--- src/kobo_highlights_export/cli.py
+++ src/kobo_highlights_export/cli.py
@@ -27,18 +27,17 @@ Command line tool for importing Kobo e-reader highlights into a Notion database.
 This tool allows you to connect your Kobo device, access the SQLite database on
 your Kobo, extract the highlights, and export them to a specified Notion database. 
 The highlights from each book are then saved in an individual page within the Notion
-database. All you need to do is follow the instructions provided by the command line tool,
-and your highlights will be automatically imported into your Notion workspace.
+database. All you need to do is follow the instructions provided by the command line 
+tool, and your highlights will be automatically imported into your Notion workspace.
 """)
     pass
 
 
 @cli.command()
-@click.option(
-    "-Filepath",
-    "--filepath",
-    "-filepath",
-    prompt="""
+def main():
+    # Echo the values to the user to confirm the input
+    filepath = click.prompt(
+        """
 Connect your Kobo to your computer and note the file path to the Kobo,
 you can copy and paste the file path here. It should look something like
 
@@ -46,15 +45,24 @@ D:/KOBOeReader or /Volumes/KOBOeReader
 
 This path is used to access the SQLite database containing your highlights.
 Enter the file path here""",
-    help="""If you are having trouble, you can provide your .kobo/KoboReader.sqlite file path specifically. This file is normally hidden inside of your KOBOeReader folder. 
-If you are unable to locate the file, please ensure that your Kobo is connected to your computer and that you have enabled the option to view hidden files in your file explorer""",
-    type=click.Path(exists=True),
-)
-@click.option(
-    "-API-Key",
-    "--api-key",
-    "-api-key",
-    prompt="""
+        type=click.Path(exists=True),
+    )
+    click.echo(f"File Path: {filepath}")
+    db_id = click.prompt("""
+                         Enter your Notion Database ID here,
+this is a part of the URL of your Notion database.
+
+For example this is the URL for a Notion database:
+https://www.notion.so/Kobo-954e1825aae14feaa6960ea59c923v24
+
+The Database ID would be: 954e1825aae14feaa6960ea59c923v24
+If you have more than one database, ensure you are using the correct one.
+You can always delete the data if it is imported into the wrong Notion database,
+so don't worry too much about making a mistake.
+Enter the Database ID""")
+    click.echo(f"Database ID: {db_id}")
+    api_key = click.prompt(
+        """
 Enter your Notion API key, this needs to be created in your Notion workspace.
 You can find it in the Integrations section of your workspace settings. 
 Once created, make sure you give the page access to the integration,
@@ -65,31 +73,8 @@ Hint: It should start with "secret_".
 Note: This key is sensitive and should not be shared with others. 
 When you enter the key, it will ask for it to be submitted twice for confirmation.
 Enter the API key here""",
-    help="Enter your Notion API key. This key is necessary for authentication to access your Notion workspace.",
-    confirmation_prompt=True,
-)
-@click.option(
-    "-Database-ID",
-    "--db-id",
-    "-db-id",
-    prompt="""
-Enter your Notion Database ID here,
-this is a part of the URL of your Notion database.
-
-For example this is the URL for a Notion database:
-https://www.notion.so/Kobo-954e1825aae14feaa6960ea59c923v24
-
-The Database ID would be: 954e1825aae14feaa6960ea59c923v24
-If you have more than one database, ensure you are using the correct one.
-You can always delete the data if it is imported into the wrong Notion database,
-so don't worry too much about making a mistake.
-Enter the Database ID""",
-    help="Enter the ID of the Notion database where you want to export the highlights. Ensure that this database is set up to receive the data structure being exported.",
-)
-def process_books(filepath, api_key, db_id):
-    # Echo the values to the user to confirm the input
-    click.echo(f"Database ID: {db_id}")
-    click.echo(f"File Path: {filepath}")
+        confirmation_prompt=True,
+    )
     """Retrieve database from the Bookmarks SQLite database from the Kobo."""
     kobo_db = KoboDatabase(filepath)
     kobo_db.connect()
@@ -103,5 +88,4 @@ def process_books(filepath, api_key, db_id):
     click.echo(f"Notion setup complete with Database ID: {db_id}")
     book_processor = NotionInsert(notion_exporter)
     book_processor.process_books(books)
-
     click.echo("Books successfully processed and added to Notion database.")
--- src/kobo_highlights_export/main.py
+++ src/kobo_highlights_export/main.py
@@ -1,34 +1,4 @@
-from dotenv import load_dotenv
-import os
-from notion import NotionExporter
-from notion_insert import NotionInsert
-from kobo import KoboDatabase
-# import utils
-
-
-load_dotenv()
-notion_api_key = os.environ.get("NOTION_API_KEY")
-notion_db_id = os.environ.get("NOTION_DB_ID")
-# notion = Client(auth=notion_api_key)
-if os.environ.get("PROD") is True:
-    ereader_db = os.environ.get("EREADER_DB")
-else:
-    db_path = os.environ.get("DEV_EREADER_DB")
-    ereader_db = os.path.join(os.path.dirname(__file__), "..", "..", db_path)
-
-
-def main():
-    """Retrieve database from the Bookmarks SQLite database from the Kobo."""
-    kobo_db = KoboDatabase(ereader_db)
-    kobo_db.connect()
-    books = kobo_db.get_highlights()
-    kobo_db.close()
-
-    """Set up the Notion exporter with API key and database ID"""
-    notion_exporter = NotionExporter(notion_api_key, notion_db_id)
-    book_processor = NotionInsert(notion_exporter)
-    book_processor.process_books(books)
-
+import cli
 
 if __name__ == "__main__":
-    main()
+    cli()
